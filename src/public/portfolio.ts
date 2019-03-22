327faf661221ed1f14a4764e0932141218a4c683
Unknown <ijavid@gmail.com>
2019-03-22 13:10:56 +0100

    double chart

--- src/public/portfolio.ts
+++ src/public/portfolio.ts
@@ -37,6 +37,13 @@ $.getJSON('api/position/values/', {}, (result: Array<Series>) => {
         return {min, max}
     }, {min: 0, max: 0});
 
+    const mm2 = result.reduce(({ min, max }, { data }) => {
+        const values = data.map( a => a.perf);
+        min = Math.min(min, ...values);
+        max = Math.max(max, ...values);
+        return {min, max}
+    }, {min: 0, max: 1});
+
     const margin = {top: 20, right: 30, bottom: 30, left: 60};
     const height = 500;
     const width = 1000;
@@ -45,9 +52,12 @@ $.getJSON('api/position/values/', {}, (result: Array<Series>) => {
         .range([margin.left, width - margin.right]);
 
     // console.log(data, x);
-
     const y = d3.scaleLinear()
         .domain([mm.min, mm.max]).nice()
+        .range([height - margin.bottom, margin.top]);
+
+    const y2 = d3.scaleLinear()
+        .domain([mm2.min, mm2.max]).nice()
         .range([height - margin.bottom, margin.top]);
 
     const xAxis = (g: any) => g
@@ -62,12 +72,27 @@ $.getJSON('api/position/values/', {}, (result: Array<Series>) => {
             .attr("x", 3)
             .attr("text-anchor", "start")
             .attr("font-weight", "bold")
-            .text('Title'));
+            .text('NetPL'));
+
+    const yAxis2 = (g: any)=> g
+        .attr("transform", `translate(${width - margin.right},0)`)
+        .call(d3.axisRight(y2))
+        .call((g: any) => g.select(".domain").remove())
+        .call((g: any) => g.select(".tick:last-of-type text").clone()
+            .attr("x", -60)
+            .attr("text-anchor", "start")
+            .attr("font-weight", "bold")
+            .text('NetPerf'));
 
     const line = d3.line<GraphValue>()
         .defined((d: GraphValue) => !isNaN(d.value))
         .x((d: GraphValue) => x(d.date))
         .y((d: GraphValue) => y(d.value));
+
+    const line2 = d3.line<GraphValue>()
+        .defined((d: GraphValue) => !isNaN(d.perf))
+        .x((d: GraphValue) => x(d.date))
+        .y((d: GraphValue) => y2(d.perf));
 
     const svg = d3.select('#chart')
         .attr("width", width)
@@ -78,6 +103,10 @@ $.getJSON('api/position/values/', {}, (result: Array<Series>) => {
 
     svg.append("g")
         .call(yAxis);
+
+    svg.append("g")
+        .call(yAxis2);
+
 
     result.forEach((item) => {
 
@@ -91,6 +120,15 @@ $.getJSON('api/position/values/', {}, (result: Array<Series>) => {
             .attr("stroke-linejoin", "round")
             .attr("stroke-linecap", "round")
             .attr("d", line);
+
+        svg.append("path")
+            .datum(data)
+            .attr("fill", "none")
+            .attr("stroke", "green")
+            .attr("stroke-width", 1.5)
+            .attr("stroke-linejoin", "round")
+            .attr("stroke-linecap", "round")
+            .attr("d", line2);
 
     });
 
